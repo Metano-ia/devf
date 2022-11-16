6ed3753a605e49d13029fbb18d36d1c25d8da71f
Metano-ia <68360029+Metano-ia@users.noreply.github.com>
2022-11-15 22:11:34 -0600

    Add files via upload

---
+++ PiedraPapelTijera/main.js
@@ -0,0 +1,50 @@
+// // PIEDRA, PAPEL O TIJERA
+// let players = [nameOne= prompt('Jugador 1: Inserta tu nombre.'), nameTwo= prompt('Jugador 2: Inserta tu nombre.'),pregunta= '¿piedra, papel o tijera?'];
+
+// let nplayerOne = prompt(nameOne+ ', '+pregunta);
+// let nplayerTwo = prompt(nameTwo+ ', '+pregunta);
+
+// let piedra = 'piedra';
+// let papel = 'papel';
+// let tijera = 'tijera';
+
+// if (nplayerOne === tijera && nplayerTwo === tijera){
+//     console.log('¡EMPATE!');
+// }
+// if (nplayerOne === tijera && nplayerTwo === papel){
+//     console.log('¡El ganador es '+nameOne+ '!');
+// }
+// if (nplayerOne === tijera && nplayerTwo === piedra){
+//     console.log('¡El ganador es '+nameTwo+ '!');
+// }
+// if (nplayerOne === papel && nplayerTwo === tijera){
+//     console.log('¡El ganador es '+nameTwo+ '!');
+// }
+// if (nplayerOne === papel && nplayerTwo === papel){
+//     console.log('¡EMPATE!');
+// }
+// if (nplayerOne === papel && nplayerTwo === piedra){
+//     console.log('¡El ganador es '+nameOne+ '!');
+// }
+// if (nplayerOne === piedra && nplayerTwo === tijera){
+//     console.log('¡El ganador es '+nameOne+ '!');
+// }
+// if (nplayerOne === piedra && nplayerTwo === papel){
+//     console.log('¡El ganador es '+nameTwo+ '!');
+// }
+// if (nplayerOne === piedra && nplayerTwo === piedra){
+//     console.log('¡EMPATE!');
+// }
+
+//Módulo: Devuelve el residuo de una división.
+
+//NÚMEROS PARES
+// let numero = prompt('Inserte un número:');
+// let resultado =  numero % 2;
+// if (resultado === 0){
+//     console.log('Es par');
+// } else {
+//     console.log('Es impar');
+// }
+ 
+//CICLOS
