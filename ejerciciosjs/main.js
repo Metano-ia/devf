eaab7279c1f762fc8300ff4ff901f5ce55a8cb8b
Metano-ia <68360029+Metano-ia@users.noreply.github.com>
2022-11-15 22:15:27 -0600

    Add files via upload

---
+++ ejerciciosjs/main.js
@@ -0,0 +1,141 @@
+// 1 - TOPPINGS
+
+// var topping= prompt("¿Qué topping quieres?");
+// var helado= 35;
+// var oreo= 12;
+// var KitKat= 17;
+// var brownie= 13;
+// var total= 0;
+
+// if(topping=="oreo"){
+//     var total= helado+oreo;
+// } else if (topping=="KitKat"){
+//     var total= helado+KitKat;
+// } else if (topping=="brownie"){
+//     var total= helado+brownie;
+// }
+
+// alert("Tu total es= "+total)
+
+// 2 - MÓDULO
+
+// var num1 = prompt("Dame un número");
+// var num2 = prompt("Dame otro número");
+
+// var total = num1%num2;
+// if(total===0){
+//     alert("Tu división es exacta (:")
+// } else{
+//     alert("Tu división no es exacta, el residuo es= " +total)
+// }
+
+// 3 - MAYOR Y MENOR
+
+// var num1 = prompt("Dame un número");
+// var num2 = prompt("Dame otro número");
+
+// if(num1===num2){
+//     alert("¡Tus números son iguales!")
+// } else if (num1<num2){
+//     alert(num1+" es menor que "+num2)
+// } else if(num1>num2){
+//     alert(num1+" es mayor que "+num2)
+// }
+
+// 4 - AÑOS
+
+// var anio1= prompt("Ingresa el año 1.");
+// var anio2= prompt("Ingresa el año 2.");
+
+// if(anio1>anio2){
+//     var pasado= anio1-anio2;
+//     alert("Han pasado "+pasado+" años")
+// } else if(anio1<anio2){
+//     var futuro= anio2-anio1;
+//     alert("Aún faltan "+futuro+" años")
+// } else if(anio1===anio2){
+//     alert("Bro, es el mismo año (:")
+// }
+
+// 5 - EL MAYOR *********+
+
+// var num1= prompt("Ingresa el número 1.");
+// var num2= prompt("Ingresa el número 2.");
+// var num3= prompt("Ingresa el número 3.");
+
+// if(num1+num2)
+
+
+
+// FOR
+
+// Imprimir del 1 al 100
+
+// for(var i=1; i<=100; i++){
+//     console.log(i);
+// }
+
+// Imprimir los números pares del 1 al 500
+
+// for(var i=1; i<=500; i++){
+//     if(i%2===0){
+//         console.log(i);
+//     }
+// }
+
+// Crea la suma de todos los números pares del 1 al 3000
+// var suma=0;
+
+// for(var i=1; i<=500; i++){
+//     if(i%2===0){
+//         suma=suma+i;
+//     }
+// }
+// console.log(suma);
+
+// Crea una serie Fibonacci hasta el 1000 - verificar
+
+// var j=0;
+
+// for(var i=0; i<=1000; i++){
+//     if(i<2){
+//         console.log(i);
+//     } else {
+//         var p=(1+Math.sqrt(5))/2;
+//         j= (Math.pow(p,i)-(Math.pow((1-p),i)))/Math.sqrt(5,2);
+//         var k= j.toFixed(0);
+//         console.log(k);
+//     }
+// }
+
+// Mostrar los multiplos de 3 del 0 al 100
+
+// for(var i=0; i<=100; i++){
+//     if(i%3===0){
+//         console.log(i);
+//     }
+// }
+
+//Pedirle al usuario un número (prompt) y mostrar los números del 1 hasta el número ingresado.
+
+// var number= prompt("Dame un número:")
+
+
+// for(var i=0; i<=number; i++){
+//     console.log(i);
+// }
+
+//Contar los multiplos de 3 desde 1 hasta un num que ingrese el usuario.
+
+// var number= prompt("Dame un número:")
+// var contador= 0;
+// for(var i=1; i<=number; i++){
+//     if(i%3===0){
+//         contador= contador+1;
+//     }
+// }
+
+// console.log(contador);
+
+// Mostrar los números primos entre el 0 y 100.
+
